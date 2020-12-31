856fc71cfb06de04130dd9d986459c9c2bc1656a
Jake Cawley <jcawl123@gmail.com>
2020-12-30 21:22:31 -0500

    candle chart added

---
+++ src/StockSearchComps/CandleChart.js
@@ -0,0 +1,22 @@
+import React from "react";
+import TradingViewWidget, { Themes } from 'react-tradingview-widget';
+
+const App = () => (
+    <TradingViewWidget
+      symbol="NASDAQ:AAPL"
+      hide_side_toolbar={false}
+      theme={Themes.DARK}
+      locale="us"
+      autosize
+    />
+  );
+
+export default class Comp1 extends React.Component{
+    render (){
+        return(
+            <div style={{"background":"yellow", "height":"500px"}}>
+                {App()}
+            </div>
+        )
+    }
+}
